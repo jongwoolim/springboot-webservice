c31dba2f6d79276bd475fe93db2e5addc5aac8cc
limjongwoo <whddn528@naver.com>
2021-01-08 18:21:07 +0900

    Initial commit

---
+++ src/main/java/org/zerock/demo/dto/SampleDTO.java
@@ -0,0 +1,16 @@
+package org.zerock.demo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}

433c386a8776fd45ef5536447897fd42826d74b8
limjongwoo <whddn528@naver.com>
2021-02-15 16:52:45 +0900

    feat: BoardService getList add

--- src/main/java/org/zerock/board/service/BoardService.java
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -1,12 +1,29 @@
 package org.zerock.board.service;
 
+
 import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
 public interface BoardService {
 
     Long register(BoardDTO dto);
+
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+
+
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+
+        return BoardDTO.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue()).build();
+
+    }
 
     default Board dtoToEntity(BoardDTO boardDTO){
 
@@ -18,5 +35,6 @@ public interface BoardService {
                     .writer(member)
                     .build();
     }
+
 
 }

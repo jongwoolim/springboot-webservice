4bcae26e69be0f1da2f89189b39d6b0223d58b62
limjongwoo <whddn528@naver.com>
2021-02-17 13:29:44 +0900

    feat: read,modify.html add

--- src/main/java/org/zerock/board/controller/BoardController.java
+++ src/main/java/org/zerock/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,6 +20,45 @@ import org.zerock.board.service.BoardService;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes rttr){
+
+        log.info("post modify.........");
+
+        boardService.modify(dto);
+
+        rttr.addAttribute("page", requestDTO.getPage());
+        rttr.addAttribute("type", requestDTO.getType());
+        rttr.addAttribute("keyword", requestDTO.getKeyword());
+
+        rttr.addAttribute("bno", dto.getBno());
+
+        return "redirect:/board/read";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes rttr){
+
+        log.info("bno: " + bno);
+
+        boardService.removeWithReplies(bno);
+        rttr.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
+
+    @GetMapping({"/read", "modify"})
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
+
+        log.info("bno: " + bno);
+
+        final BoardDTO boardDTO = boardService.get(bno);
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+    }
 
     @PostMapping("/register")
     public String registerPost(BoardDTO dto, RedirectAttributes rttr){
